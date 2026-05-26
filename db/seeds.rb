ddaf6d9651e75ef52e3f7b5c402659c7698e0785
Alex Cheng <alexndrc25@gmail.com>
2026-05-26 17:13:15 +0200

    Create seeds for user schedule and task

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "destroying user..."
+User.destroy_all
+puts "creating user..."
+
+user = User.create!(email: 'alex@example.com', password: '123123')
+
+puts "creating schedule..."
+
+schedule_1 = Schedule.find_or_create_by!(date: Date.today, user: user)
+schedule_2 = Schedule.find_or_create_by!(date: Date.tomorrow, user: user)
+schedule_3 = Schedule.find_or_create_by!(date: Date.yesterday, user: user)
+
+puts "creating task..."
+
+[schedule_1, schedule_2, schedule_3].each do |schedule|
+  Task.create!(category: 'work', duration_min: 60, fixed_time: "8:00", preferred_time: "10:00", schedule: schedule)
+  Task.create!(category: 'free time', duration_min: 90, fixed_time: "18:00", preferred_time: "20:00", schedule: schedule)
+  Task.create!(category: 'go for a walk', duration_min: 120, fixed_time: "19:00", preferred_time: "2:00", schedule: schedule)
+  Task.create!(category: 'eat cookies', duration_min: 30, fixed_time: "21:00", preferred_time: "4:00", schedule: schedule)
+end
+
+puts "Finished!"
